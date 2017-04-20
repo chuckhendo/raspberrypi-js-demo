1e09fd65d39cb283f85075333e00f78023b4b803
Carl Henderson <chuckhendo@gmail.com>
2017-04-20 13:29:51 -0400

    promise.all receives an array

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const gpio = require("rpi-gpio");
 
 gpio.setMode(gpio.MODE_BCM);
 
-function promiseSetupPin(pin, direction) {
+function setupPin(pin, direction) {
     return new Promise((resolve, reject) => {
         gpio.setup(pin, direction, resolve);
     });
 }
 
-Promise.all(promiseSetupPin(18, gpio.DIR_OUT)).then(() => {
-    gpio.write(18, true);
+Promise.all([setupPin(24, gpio.DIR_OUT)]).then(() => {
+    gpio.write(24, true);
 });
